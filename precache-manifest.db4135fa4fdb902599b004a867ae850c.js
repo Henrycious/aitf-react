b787a918f2ad7acedb2ca156d8c79dc12d0b5c73
Henry Gennet <henry.gennet@gmail.com>
2022-10-20 18:31:27 +0200

    Updates

---
+++ precache-manifest.db4135fa4fdb902599b004a867ae850c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "02e9da94509d0b45217c",
+    "url": "/aitf-react/static/css/main.a2f8fc36.chunk.css"
+  },
+  {
+    "revision": "02e9da94509d0b45217c",
+    "url": "/aitf-react/static/js/main.02e9da94.chunk.js"
+  },
+  {
+    "revision": "6ea3fd4c157af86df7ec",
+    "url": "/aitf-react/static/js/1.6ea3fd4c.chunk.js"
+  },
+  {
+    "revision": "5ff485ad9b93da03c7bd",
+    "url": "/aitf-react/static/js/runtime~main.5ff485ad.js"
+  },
+  {
+    "revision": "1ff3499c0486707f3af29aec790b63b2",
+    "url": "/aitf-react/static/media/Platzhalter_1.1ff3499c.png"
+  },
+  {
+    "revision": "f49a6c170d29ebd39c7afd512d176396",
+    "url": "/aitf-react/static/media/aitf.f49a6c17.png"
+  },
+  {
+    "revision": "eafc419a1905d2a983e1a79317bfad6c",
+    "url": "/aitf-react/static/media/aitf_hero_1.eafc419a.png"
+  },
+  {
+    "revision": "e1162e59603e7eb6f31e45587b32c909",
+    "url": "/aitf-react/static/media/traktor_mensch_kuh.e1162e59.png"
+  },
+  {
+    "revision": "e7a5bb803b390b9a7012c97efcacccc0",
+    "url": "/aitf-react/static/media/GIL.e7a5bb80.png"
+  },
+  {
+    "revision": "e3533b176d278ce89d5a2caa6349478a",
+    "url": "/aitf-react/static/media/Platzhalter_2.e3533b17.png"
+  },
+  {
+    "revision": "e351b3fdfbec35d9bc4ef075c39953d7",
+    "url": "/aitf-react/static/media/Platzhalter_3.e351b3fd.png"
+  },
+  {
+    "revision": "8e0a0e1fd9044a4eccb2f87e2742e593",
+    "url": "/aitf-react/static/media/Schild_AI_TEST_FIELD.8e0a0e1f.png"
+  },
+  {
+    "revision": "98ed68b160831d0d12fd8d418b1443bc",
+    "url": "/aitf-react/index.html"
+  }
+];
